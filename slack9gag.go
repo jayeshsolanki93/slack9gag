02be7cc8a434c87a6859bf4edeaacb3af4a64fb6
jayeshsolanki93 <jayeshsolanki93@gmail.com>
2016-03-10 00:47:23 +0530

    Reading the 9gag API and returning the formatted json for the bot.

--- slack9gag.go
+++ slack9gag.go
@@ -3,9 +3,46 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
 )
+
+type Response struct {
+	Text        string
+	Attachments []Attachments
+}
+
+type Attachments struct {
+	Title      string
+	Title_link string
+	Thumb_url  string
+}
+
+type jsonData struct {
+	Status  int64
+	Message string
+	Data    []Data
+}
+
+type Data struct {
+	Id      string
+	Caption string
+	Images  struct {
+		Small  string
+		Cover  string
+		Normal string
+		Large  string
+	}
+	Media interface{}
+	Link  string
+	Votes struct {
+		Count int64
+	}
+	Comments struct {
+		Count int64
+	}
+}
 
 func main() {
 	http.HandleFunc("/", handler)
@@ -21,15 +58,45 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	command := r.FormValue("command")
 
 	if command == "/9gag" {
-		jsonResp(w, "Hello World")
+		r, err := http.Get("http://infinigag.k3min.eu")
+		if err != nil {
+			fmt.Println("Error requesting data")
+			return
+		}
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			fmt.Println("Error while opening file", err)
+			return
+		}
+		x := new(jsonData)
+		err = json.Unmarshal(body, &x)
+		if err != nil {
+			fmt.Println("Error while parsing file", err)
+			return
+		}
+		jsonResp(w, x)
 	} else {
 		fmt.Fprint(w, "I do not understand your command.")
 	}
 }
 
-func jsonResp(w http.ResponseWriter, msg string) {
+func jsonResp(w http.ResponseWriter, x *jsonData) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	resp := map[string]string{"text": msg}
+
+	attachments := make([]Attachments, 10)
+	for i := 0; i < 9; i++ {
+		attachments[i] = Attachments{
+			Title:      x.Data[i].Caption,
+			Title_link: x.Data[i].Link,
+			Thumb_url:  x.Data[i].Images.Small,
+		}
+	}
+
+	resp := Response{
+		Text:        "lorem ipsum",
+		Attachments: attachments,
+	}
+
 	r, err := json.Marshal(resp)
 	if err != nil {
 		fmt.Println("Couldn't marshal hook response:", err)
